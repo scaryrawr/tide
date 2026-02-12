526a13c96b9afd8aab366f9fc2f560c93deecab0
Mike Wallio <michael.wallio@outlook.com>
2026-02-12 21:25:55 +0000

    tests: stabilize output from local Tide config

--- tests/_tide_item_gh_user.test.fish
+++ tests/_tide_item_gh_user.test.fish
@@ -1,19 +1,14 @@
 # RUN: %fish %s
+_tide_parent_dirs
 
 function _gh_user
     _tide_decolor (_tide_item_gh_user)
 end
 
+set -l tmpdir (mktemp -d)
+set -lx HOME $tmpdir
 set -lx tide_gh_user_icon 
-
 set -l gh_hosts_file $HOME/.config/gh/hosts.yml
-set -l backup_hosts_file
-
-# Backup existing hosts.yml if it exists
-if test -f $gh_hosts_file
-    set backup_hosts_file (mktemp)
-    cp $gh_hosts_file $backup_hosts_file
-end
 
 # No hosts.yml → no output
 command rm -f $gh_hosts_file
@@ -25,10 +20,4 @@ printf 'github.com:\n    git_protocol: https\n    users:\n        testuser:\n   
 
 _gh_user # CHECK:  testuser
 
-# Restore original hosts.yml or remove test file
-if test -n "$backup_hosts_file"
-    cp $backup_hosts_file $gh_hosts_file
-    command rm -f $backup_hosts_file
-else
-    command rm -f $gh_hosts_file
-end
+command rm -r $tmpdir
