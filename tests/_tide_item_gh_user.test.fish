9f8a785adec5dfcce13fbdca97325bc69cc75eae
Mike Wallio <michael.wallio@outlook.com>
2026-02-12 17:12:17 +0000

    Add GitHub user icon and update related test

--- tests/_tide_item_gh_user.test.fish
+++ tests/_tide_item_gh_user.test.fish
@@ -1,6 +1,11 @@
-# Test for _tide_item_gh_user
+# RUN: %fish %s
 
-set -l test_hosts_file (mktemp)
+function _gh_user
+    _tide_decolor (_tide_item_gh_user)
+end
+
+set -lx tide_gh_user_icon 
+
 set -l gh_hosts_file $HOME/.config/gh/hosts.yml
 set -l backup_hosts_file
 
@@ -10,30 +15,20 @@ if test -f $gh_hosts_file
     cp $gh_hosts_file $backup_hosts_file
 end
 
-# Prepare a mock hosts.yml
-mkdir -p ~/.config/gh
-printf 'github.com:\n    git_protocol: https\n    users:\n        testuser:\n    user: testuser\n' > $test_hosts_file
-cp $test_hosts_file $gh_hosts_file
+# No hosts.yml → no output
+command rm -f $gh_hosts_file
+_gh_user # CHECK:
 
-# Set icon and color for test
-set -g tide_gh_user_icon 'ï'
-set -g tide_gh_user_color 0087AF
-set -g tide_gh_user_bg_color normal
+# Create a mock hosts.yml
+mkdir -p (path dirname $gh_hosts_file)
+printf 'github.com:\n    git_protocol: https\n    users:\n        testuser:\n    user: testuser\n' >$gh_hosts_file
 
-# Capture output
-set -g _tide_side right
-set -l output (_tide_item_gh_user)
-
-# Clean up test files
-rm $test_hosts_file
+_gh_user # CHECK:  testuser
 
 # Restore original hosts.yml or remove test file
 if test -n "$backup_hosts_file"
     cp $backup_hosts_file $gh_hosts_file
-    rm $backup_hosts_file
+    command rm -f $backup_hosts_file
 else
-    rm $gh_hosts_file
+    command rm -f $gh_hosts_file
 end
-
-# Assert output contains icon and username
-string match -q '*ï testuser*' -- $output
